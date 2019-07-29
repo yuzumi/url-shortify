7b2556425a434139c57828ad07d5437752be92c7
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-29 09:35:45 +0300

    Connect to database

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+
+import connectToDatabase from './database/connect';
+
+dotenv.config();
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi');
-});
+const { 
+  PORT = 5000,
+  DATABASE_URL,
+  DATABASE_NAME,  
+} = process.env;
 
-app.listen(5000, () => {
-  console.log('Server running');
+connectToDatabase(`${DATABASE_URL}${DATABASE_NAME}`);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
