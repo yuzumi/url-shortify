d9f338995716dca771be37da99534122965b3790
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-29 10:26:12 +0300

    Define routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import dotenv from 'dotenv';
 
 import connectToDatabase from './database/connect';
+import UrlRouter from './routes/url';
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ connectToDatabase(`${DATABASE_URL}${DATABASE_NAME}`);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/', UrlRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
