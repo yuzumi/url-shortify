fa7baddd9618911dd686c675da34f7f0393d18f7
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-31 11:07:01 +0300

    Create an application module

---
+++ src/app.module.ts
@@ -0,0 +1,52 @@
+import express, { Application } from 'express';
+import config from 'config';
+import mongoose from 'mongoose';
+
+import loggerMiddleware from './middleware/logger.middleware';
+import Controller from './interfaces/controller.interface';
+
+export default class App {
+  private app: Application;
+  private port: number;
+
+  constructor(controllers: Controller[], port: number) {
+    this.app = express();
+    this.port = port;
+
+    this.connectToDatabase();
+    this.initializeMiddleware();
+    this.initializeControllers(controllers);
+  }
+
+  private initializeMiddleware(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+
+    this.app.use(loggerMiddleware);
+  }
+
+  private initializeControllers(controllers: Controller[]): void {
+    controllers.forEach(controller => {
+      this.app.use(controller.path, controller.router);
+    });
+  }
+
+  private async connectToDatabase(): Promise<void> {
+    try {
+      const uri: string = config.get('database.uri');
+
+      await mongoose.connect(uri, { useNewUrlParser: true });
+  
+      console.log('Database connected');
+    } catch (error) {
+      console.error(error.message);
+      process.exit(1);    
+    }
+  }
+
+  public listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`);
+    });
+  }
+}
